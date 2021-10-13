da33c4cd221c5a3b1222435dd04e0dadcafae735
chanchann <www.769440972@qq.com>
2021-10-14 00:58:56 +0800

    fix 559 issue demo bug

--- demos/26_resource_pool/26_issue_559_server.cc
+++ demos/26_resource_pool/26_issue_559_server.cc
@@ -11,6 +11,7 @@
 static WFFacilities::WaitGroup wait_group(1);
 std::atomic<int> cnt;
 std::unordered_map<int, std::unique_ptr<WFResourcePool> > uid_pool_map;
+std::mutex mutex;
 
 void sig_handler(int signo)
 {
@@ -24,6 +25,10 @@ void go_func(int uid, protocol::HttpResponse *resp)
     resp->append_output_body(buf);
 }
 
+struct server_context {
+    int uid;
+};
+
 int main()
 {
     signal(SIGINT, sig_handler);
@@ -36,12 +41,16 @@ int main()
         size_t len;
         req->get_parsed_body(&body, &len);
 
-        int uid = atoi(static_cast<const char *>(body));
-        fprintf(stderr, "uid : %d\n", uid);
- 
-        if (!uid_pool_map.count(uid))
+        const int uid = atoi(static_cast<const char *>(body));
+        server_context *ctx = new server_context;
+        ctx->uid = uid;
+        server_task->user_data = ctx;
         {
-            uid_pool_map[uid] = util::make_unique<WFResourcePool>(1);
+            std::lock_guard<std::mutex> lock(mutex);
+            if (!uid_pool_map.count(uid))
+            {
+                uid_pool_map[uid] = util::make_unique<WFResourcePool>(1);
+            }
         }
      
         WFGoTask *go_task = WFTaskFactory::create_go_task("GO", go_func, uid, resp);
@@ -49,11 +58,19 @@ int main()
         WFConditional *cond = uid_pool_map[uid]->get(go_task, &go_task->user_data);
         series_of(server_task)->push_back(cond);
 
-        server_task->set_callback([&uid](WFHttpTask *)
+        // 此处有个离谱的坑，此处捕获的uid已经变了，因为这个是在process函数完了再进行
+        // server_task->set_callback([&uid](WFHttpTask *)
+        // { 
+        //     uid_pool_map[uid]->post(nullptr); 
+        //     fprintf(stderr, "post resource back\n");
+        // });
+        server_task->set_callback([](WFHttpTask *server_task)
         { 
+            auto ctx = static_cast<server_context *>(server_task->user_data);
+            uid_pool_map[ctx->uid]->post(nullptr); 
             fprintf(stderr, "post resource back\n");
-            uid_pool_map[uid]->post(nullptr); 
         });
+
     });
 
     if (server.start(8888) == 0)
